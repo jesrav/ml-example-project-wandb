cde523ed1d1712bd237499fdfd7221a6bde4a004
jes <jesravnbol@hotmail.com>
2022-01-29 21:16:52 +0100

    wip

--- src/models/promote_model.py
+++ src/models/promote_model.py
@@ -5,6 +5,7 @@ import sys
 from tempfile import TemporaryDirectory
 
 import hydra
+import joblib
 import wandb
 
 from src.logger import logger
@@ -50,16 +51,24 @@ def main(config):
     )
 
     logger.info("Loading latest trained model.")
-    latest_model = get_model(
+    latest_model_artifact = get_model_artifact(
         project_name=config["main"]["project_name"],
         model_name=config['artifacts']['model']['name'],
         model_version="latest"
     )
+    if "prod" in latest_model_artifact.aliases:
+        logger.warning(
+            f"Latest model is already the production model. No new model to promote."
+        )
+        return
+    run.use_artifact(latest_model_artifact)
+    latest_model_path = latest_model_artifact.file()
+    latest_model = joblib.load(latest_model_path)
 
     logger.info("Predictimng on hold out test set to get model performance of latest trained model.")
     predictions = latest_model.predict(test_data)
     evaluation = RegressionEvaluation(
-        y_true=config["main"]["target_column"],
+        y_true=test_data[config["main"]["target_column"]],
         y_pred=predictions
     )
     model_performance = evaluation.get_metrics()["mae"]
@@ -67,7 +76,7 @@ def main(config):
 
     if evaluation.get_metrics()["mae"] > config["main"]["max_mae_to_promote"]:
         logger.warning(
-            f"Trained model has MAE of {model_performance}, which is not below maximum MAE of {performance_th}."
+            f"Trained model has MAE of {model_performance}, which is not below maximum MAE of {performance_th}. "
             f"Model is not promoted."
         )
         return
@@ -78,11 +87,14 @@ def main(config):
 
     logger.info("Loading current prod model.")
     try:
-        current_prod_model = get_model(
+        current_prod_model_artifact = get_model_artifact(
             project_name=config["main"]["project_name"],
             model_name=config['artifacts']['model']['name'],
-            model_version="latest"
+            model_version="prod"
         )
+        run.use_artifact(current_prod_model_artifact)
+        current_prod_model_path = current_prod_model_artifact.file()
+        current_prod_model = joblib.load(current_prod_model_path)
     except ModelDoesNoteExistError:
         logger.info("No current production model. Promoting model without comparing.")
         promote_model(
@@ -95,11 +107,11 @@ def main(config):
     logger.info("Predictimng on hold out test set to get model performance of current prod model.")
     predictions_prod = current_prod_model.predict(test_data)
     evaluation_prod = RegressionEvaluation(
-        y_true=config["main"]["target_column"],
+        y_true=test_data[config["main"]["target_column"]],
         y_pred=predictions_prod
     )
     model_performance_prod = evaluation_prod.get_metrics()["mae"]
-    if model_performance > model_performance_prod:
+    if model_performance < model_performance_prod:
         logger.info("Latest model has better performance. Promoting latest model to prod.")
         promote_model(
             project_name=config["main"]["project_name"],
@@ -108,8 +120,8 @@ def main(config):
         )
     else:
         logger.warning(
-            f"Trained model has MAE of {model_performance}, which is not lower than current prod model, "
-            f"with a MAE of {model_performance_prod}."
+            f"Latest trained model has MAE of {model_performance}, which is not lower than current prod model, "
+            f"with a MAE of {model_performance_prod}. "
             f"Model is not promoted."
         )
 
